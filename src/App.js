345af9e58b0b29888a74e6a85bb6d0dcdb401602
AaquibSyed <m.nsyedaaquib@gmail.com>
2021-01-05 13:25:21 +0530

    puzzle-component-layout done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import "./App.css";
 import Header from "./Components/Header";
+import Puzzle from "./Components/Puzzle";
 
 function App() {
   return (
     <div className="app">
       <Header />
-      <div className="puzzle-container">puzzzzle</div>
+      <Puzzle />
       <div className="info-container">info</div>
       <div className="input-container">input</div>
     </div>
---
+++ src/Components/Puzzle.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Puzzle.css";
+function Puzzle() {
+  return (
+    <div className="puzzle">
+      <div className="puzzle__categories">Categories here</div>
+      <div className="puzzle__box">puzzle box here</div>
+    </div>
+  );
+}
+
+export default Puzzle;
