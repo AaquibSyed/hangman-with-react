d06545b9d532b4722e4350daa89e6c55bc8179ac
AaquibSyed <m.nsyedaaquib@gmail.com>
2021-01-05 13:02:24 +0530

    redundant-header-div-removed

--- src/App.js
+++ src/App.js
@@ -4,9 +4,7 @@ import Header from "./Components/Header";
 function App() {
   return (
     <div className="app">
-      <div className="header-container">
-        <Header />
-      </div>
+      <Header />
       <div className="puzzle-container">puzzzzle</div>
       <div className="info-container">info</div>
       <div className="input-container">input</div>
