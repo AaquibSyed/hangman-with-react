973cc2aef987ac77d9c804dd8360e6ed38e0a3d7
AaquibSyed <m.nsyedaaquib@gmail.com>
2021-01-05 12:55:16 +0530

    header component completed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import "./App.css";
+import Header from "./Components/Header";
 
 function App() {
   return (
     <div className="app">
-      <div className="header-container">Header</div>
+      <div className="header-container">
+        <Header />
+      </div>
       <div className="puzzle-container">puzzzzle</div>
       <div className="info-container">info</div>
       <div className="input-container">input</div>
